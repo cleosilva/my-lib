b64266cefa6a5ebbe05c06044aebf9dd2331451b
Cleo Silva <cleo_silva1906@hotmail.com>
2023-12-12 19:13:00 -0300

    Error handling

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,40 @@
 import chalk from "chalk";
+import fs from 'fs';
 import getFile from "./index.js";
 
 const path = process.argv;
 
-async function textProcess(path) {
-    const result = await getFile(path[2]);
-    console.log(chalk.yellow('Link List'), result)
+function printList(result, identifier = '') {
+    console.log(
+        chalk.yellow('Link List'),
+        chalk.black.bgGreen(identifier),
+        result
+
+    );
+}
+
+async function textProcess(args) {
+    const path = args[2];
+
+    try {
+        fs.lstatSync(path)
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            console.log('Invalid Path or Directory');
+            return;
+        }
+    }
+
+    if (fs.lstatSync(path).isFile()) {
+        const result = await getFile(path);
+        printList(result)
+    } else if (fs.lstatSync(path).isDirectory()) {
+        const files = await fs.promises.readdir(path);
+        files.forEach(async (fileName) => {
+            const list = await getFile(`${path}/${fileName}`);
+            printList(list, fileName);
+        });
+    }
 }
 
 textProcess(path)
